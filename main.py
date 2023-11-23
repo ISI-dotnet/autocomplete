459fffc37ff38ad28b57d80db3726de6cd580067
Ech0ke <aidas.zygas@gmail.com>
2023-11-23 11:02:40 +0200

    Disable all text select after autocomplete

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class AutocompleteTextbox:
     def autocomplete(self, event):
         current_text = self.entry.get()
         words_list = current_text.split()  # Split input into individual words
-        if len(words_list) > 1:
+        if len(words_list) >= 1:
             # Get the last word for autocompletion
             last_word_to_complete = words_list[-1]
             suggestions = self.get_word_suggestions(last_word_to_complete)
@@ -28,6 +28,7 @@ class AutocompleteTextbox:
                 self.entry.insert(tk.END, completed_text)
             else:
                 messagebox.showinfo("Autocomplete", "No suggestion available")
+            return 'break'
 
     def get_word_suggestions(self, current_text):
         return [word for word in self.words if word.startswith(current_text.lower())]
